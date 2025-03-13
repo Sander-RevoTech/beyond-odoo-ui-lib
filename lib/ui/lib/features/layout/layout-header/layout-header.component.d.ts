843033611cbd97900f7b3a520c1e28dece752694
pikadjou <pikadjou@gmail.com>
2025-03-13 22:30:09 +0100

    fix --

--- lib/ui/lib/features/layout/layout-header/layout-header.component.d.ts
+++ lib/ui/lib/features/layout/layout-header/layout-header.component.d.ts
@@ -2,5 +2,5 @@ import * as i0 from "@angular/core";
 export declare class LayoutHeaderComponent {
     constructor();
     static ɵfac: i0.ɵɵFactoryDeclaration<LayoutHeaderComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<LayoutHeaderComponent, "byd--layout-header", never, {}, {}, never, ["*"], true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<LayoutHeaderComponent, "byd-layout-header", never, {}, {}, never, ["*"], true, never>;
 }
--- projects/ui/src/lib/features/layout/layout-header/layout-header.component.ts
+++ projects/ui/src/lib/features/layout/layout-header/layout-header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'byd--layout-header',
+  selector: 'byd-layout-header',
   templateUrl: './layout-header.component.html',
   styleUrls: ['./layout-header.component.scss'],
   standalone: true,
