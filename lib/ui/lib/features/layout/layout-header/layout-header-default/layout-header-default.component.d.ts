c6696496691457f62f2f539add445d094637f4ae
pikadjou <pikadjou@gmail.com>
2025-03-13 22:21:51 +0100

    update default header

--- lib/ui/lib/features/layout/layout-header/layout-header-default/layout-header-default.component.d.ts
+++ lib/ui/lib/features/layout/layout-header/layout-header-default/layout-header-default.component.d.ts
@@ -3,5 +3,5 @@ import * as i0 from "@angular/core";
 export declare class LayoutHeaderDefaultComponent extends BydBaseComponent {
     goToHome(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<LayoutHeaderDefaultComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<LayoutHeaderDefaultComponent, "byd-layout-header-default", never, {}, {}, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<LayoutHeaderDefaultComponent, "byd-layout-header-default", never, {}, {}, never, ["*"], true, never>;
 }
