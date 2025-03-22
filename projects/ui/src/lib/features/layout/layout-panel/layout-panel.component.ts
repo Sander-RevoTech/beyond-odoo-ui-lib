0acd0c45c6b3620abf077298dd0de898156fb423
pikadjou <pikadjou@gmail.com>
2025-03-22 19:17:47 +0100

    byd-layout-panel

---
+++ projects/ui/src/lib/features/layout/layout-panel/layout-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'byd-layout-panel',
+  templateUrl: './layout-panel.component.html',
+  styleUrls: ['./layout-panel.component.scss'],
+})
+export class BydLayoutPanelComponent {
+  constructor() {}
+}
--- projects/ui/src/lib/features/layout/public-api.ts
+++ projects/ui/src/lib/features/layout/public-api.ts
@@ -8,3 +8,7 @@ export * from './layout-nav/layout-nav.component';
 
 export * from './with-nav/layout-with-nav.component';
 export * from './layout-page/layout-page.component';
+export * from './layout-panel/layout-panel.component';
+export * from './with-panel/layout-with-panel.component';
+
+
