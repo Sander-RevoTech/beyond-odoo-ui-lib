3c426b02a6080da2e7719c7dba857f01e0f8ce54
pikadjou <pikadjou@gmail.com>
2025-03-22 19:07:07 +0100

    BydLayoutWithPanelComponent

---
+++ projects/ui/src/lib/features/layout/with-panel/layout-with-panel.component.ts
@@ -0,0 +1,57 @@
+import { NgClass } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatDrawer, MatDrawerContainer } from '@angular/material/sidenav';
+import { BydBaseComponent } from '@beyond/utils';
+
+
+@Component({
+  selector: 'byd-layout-with-panel',
+  templateUrl: './layout-with-panel.component.html',
+  styleUrls: ['./layout-with-panel.component.scss'],
+  standalone: true,
+  imports: [MatDrawer, MatDrawerContainer, MatIcon, NgClass],
+})
+export class BydLayoutWithPanelComponent extends BydBaseComponent implements OnChanges, AfterViewInit {
+  @Input()
+  open!: boolean;
+
+  @Input()
+  width: string = '100%';
+
+  @Output()
+  close = new EventEmitter();
+
+  @ViewChild('drawer') drawer: MatDrawer | null = null;
+
+  constructor() {
+    super();
+  }
+
+  public ngAfterViewInit() {
+    this.manageDrawer();
+    if (this.drawer) {
+      this._registerSubscription(this.drawer.closedStart.subscribe(() => this.close.emit()));
+    }
+  }
+  public ngOnChanges(changes: SimpleChanges) {
+    this.manageDrawer();
+  }
+
+  public manageDrawer() {
+    if (this.open === true) {
+      this.drawer?.open();
+    } else {
+      this.drawer?.close();
+    }
+  }
+}
