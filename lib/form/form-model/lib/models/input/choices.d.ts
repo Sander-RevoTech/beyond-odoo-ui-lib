9c4689875c24de828b6bec779b9192090b3f203f
pikadjou <pikadjou@gmail.com>
2025-03-25 20:09:41 +0100

    IInputChoices

--- lib/form/form-model/lib/models/input/choices.d.ts
+++ lib/form/form-model/lib/models/input/choices.d.ts
@@ -1,29 +1,7 @@
-import { TemplateRef } from '@angular/core';
-import { Observable } from 'rxjs';
 import { IInputDropdown, InputDropdown } from './dropdown';
-export type InputChoicesOption = {
-    id: string;
-    name: string;
-    disabled?: boolean;
-    data: any;
-};
-export interface IInputChoices extends IInputDropdown<string[]> {
-    onlyTemplate?: boolean;
-    options?: Observable<InputChoicesOption[]>;
-    advancedSearch$?: (search?: string) => Observable<InputChoicesOption[]>;
-    choiceTemplate?: {
-        one?: TemplateRef<any>;
-        list?: TemplateRef<any>;
-    };
+export interface IInputChoices<T> extends IInputDropdown<T> {
 }
-export declare class InputChoices extends InputDropdown<string[]> {
+export declare class InputChoices<T = string | string[]> extends InputDropdown<T> {
     controlType: string;
-    options: Observable<InputChoicesOption[]>;
-    onlyTemplate?: boolean;
-    advancedSearch$: ((search?: string) => Observable<InputChoicesOption[]>) | null;
-    choiceTemplate?: {
-        one?: TemplateRef<any>;
-        list?: TemplateRef<any>;
-    };
-    constructor(options?: IInputChoices);
+    constructor(options?: IInputChoices<T>);
 }
--- projects/form/form-model/src/lib/models/input/choices.ts
+++ projects/form/form-model/src/lib/models/input/choices.ts
@@ -1,36 +1,10 @@
-import { TemplateRef } from '@angular/core';
-
-import { Observable } from 'rxjs';
-
 import { IInputDropdown, InputDropdown } from './dropdown';
 
-export type InputChoicesOption = { id: string; name: string; disabled?: boolean; data: any };
-
-export interface IInputChoices extends IInputDropdown<string[]> {
-  onlyTemplate?: boolean;
-  options?: Observable<InputChoicesOption[]>;
-  advancedSearch$?: (search?: string) => Observable<InputChoicesOption[]>;
-  choiceTemplate?: {
-    one?: TemplateRef<any>;
-    list?: TemplateRef<any>;
-  };
-}
-export class InputChoices extends InputDropdown<string[]> {
+export interface IInputChoices<T> extends IInputDropdown<T> {}
+export class InputChoices<T = string | string[]> extends InputDropdown<T> {
   override controlType = 'choices';
-  declare options: Observable<InputChoicesOption[]>;
 
-  public onlyTemplate?: boolean;
-  public advancedSearch$: ((search?: string) => Observable<InputChoicesOption[]>) | null;
-  public choiceTemplate?: {
-    one?: TemplateRef<any>;
-    list?: TemplateRef<any>;
-  };
-
-  constructor(options: IInputChoices = {}) {
+  constructor(options: IInputChoices<T> = {}) {
     super(options);
-
-    this.onlyTemplate = options.onlyTemplate;
-    this.advancedSearch$ = options['advancedSearch$'] || null;
-    this.choiceTemplate = options.choiceTemplate;
   }
 }
