c4cb7eade349fbbf41f9760502808041d3218fd3
pikadjou <pikadjou@gmail.com>
2025-05-06 19:55:32 +0200

    export attachment model

--- lib/odoo/lib/features/chatting/public-api.d.ts
+++ lib/odoo/lib/features/chatting/public-api.d.ts
@@ -2,3 +2,4 @@ export * from './components/messages/messages.component';
 export * from './components/upload/files-upload.component';
 export * from './services/messages/messages.service';
 export * from './services/messages/attachments.service';
+export * from './services/messages/dto/attachment';
--- projects/odoo/src/lib/features/chatting/public-api.ts
+++ projects/odoo/src/lib/features/chatting/public-api.ts
@@ -4,3 +4,6 @@ export * from './components/upload/files-upload.component';
 export * from './services/messages/messages.service';
 export * from './services/messages/attachments.service';
 
+export * from './services/messages/dto/attachment';
+
+
