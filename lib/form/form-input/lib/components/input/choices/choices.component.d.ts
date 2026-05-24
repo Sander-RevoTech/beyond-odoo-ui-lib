418a501abf8bfcf1b952c8cd00e64579ef4bdce9
Geoffrey L <geoffrey@g-lambert.be>
2026-05-24 11:41:55 +0200

    rebuild

--- lib/form/form-input/lib/components/input/choices/choices.component.d.ts
+++ lib/form/form-input/lib/components/input/choices/choices.component.d.ts
@@ -1,10 +1,11 @@
+import { OnInit } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { InputChoices } from '@beyond/form-model';
 import { BydAbstractComponent } from '@beyond/utils';
 import * as i0 from "@angular/core";
-export declare class BydInputChoicesComponent extends BydAbstractComponent {
+export declare class BydInputChoicesComponent extends BydAbstractComponent implements OnInit {
     private _bottomSheet;
     input: InputChoices;
     matcher: ErrorStateMatcher;
@@ -14,6 +15,7 @@ export declare class BydInputChoicesComponent extends BydAbstractComponent {
         name: string;
     } | null;
     constructor(_bottomSheet: MatBottomSheet);
+    ngOnInit(): void;
     openBottomSheet(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<BydInputChoicesComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<BydInputChoicesComponent, "byd-input-choices", never, { "input": { "alias": "input"; "required": false; }; "matcher": { "alias": "matcher"; "required": false; }; }, {}, never, never, true, never>;
